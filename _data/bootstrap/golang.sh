8815b4b51985ce7567b8074269e9c828a4547bb1
fortishield <161459699+FortiShield@users.noreply.github.com>
2025-03-17 21:11:27 +0600

    Create golang.sh
    Signed-off-by: fortishield <161459699+FortiShield@users.noreply.github.com>

---
+++ _data/bootstrap/golang.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+
+. /etc/profile
+
+# renovate: datasource=golang-version depName=go
+GOLANG_VERSION=1.24.0
+GOLANG_DOWNLOAD_URL=https://golang.org/dl/go$GOLANG_VERSION.linux-$(dpkg --print-architecture).tar.gz
+
+curl -fsSL "$GOLANG_DOWNLOAD_URL" -o golang.tar.gz \
+    && tar -C /usr/local -xzf golang.tar.gz \
+    && rm golang.tar.gz
