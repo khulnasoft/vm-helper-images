ccc1728574c8ce4bfbb44aa54f3cdc8ae3124eed
fortishield <161459699+FortiShield@users.noreply.github.com>
2025-03-17 21:11:00 +0600

    Create kind.sh
    Signed-off-by: fortishield <161459699+FortiShield@users.noreply.github.com>

---
+++ _data/bootstrap/kind.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+
+. /etc/profile
+
+# renovate: datasource=github-releases depName=kubernetes-sigs/kind
+KIND_VERSION=v0.27.0
+
+GOBIN=/usr/local/bin go install sigs.k8s.io/kind@${KIND_VERSION}
