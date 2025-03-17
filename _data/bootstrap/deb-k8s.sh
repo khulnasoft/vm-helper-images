b8195dbf07260577424bd248bd53e111fb6c5dc5
fortishield <161459699+FortiShield@users.noreply.github.com>
2025-03-17 21:12:28 +0600

    Create deb-k8s.sh
    Signed-off-by: fortishield <161459699+FortiShield@users.noreply.github.com>

---
+++ _data/bootstrap/deb-k8s.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+
+. /etc/profile
+
+# renovate: datasource=github-releases depName=kubernetes/kubernetes
+KUBECTL_VERSION=1.32.2
+ARCH=$(dpkg --print-architecture)
+
+curl -LO "https://dl.k8s.io/release/v$KUBECTL_VERSION/bin/linux/$ARCH/kubectl"
+
+# validate the binary
+curl -LO "https://dl.k8s.io/release/v$KUBECTL_VERSION/bin/linux/$ARCH/kubectl.sha256"
+echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
+
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+# remove temp files
+rm kubectl kubectl.sha256
