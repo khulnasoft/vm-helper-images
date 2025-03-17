9bf8e5a9672b5885ccde2ddc7f11dd492be74db2
fortishield <161459699+FortiShield@users.noreply.github.com>
2025-03-17 21:13:13 +0600

    Create deb-docker.sh
    Signed-off-by: fortishield <161459699+FortiShield@users.noreply.github.com>

---
+++ _data/bootstrap/deb-docker.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+
+. /etc/profile
+
+curl -fsSL https://download.docker.com/linux/debian/gpg | \
+      gpg --dearmor --yes -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+chmod 0644 /usr/share/keyrings/docker-archive-keyring.gpg
+
+cat > /etc/apt/sources.list.d/docker.list <<EOF
+deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian bullseye stable
+EOF
+
+apt-get update --quiet && apt-get install --quiet --yes --no-install-recommends \
+    docker-ce docker-ce-cli containerd.io
+
+# for iptables 1.8.8
+update-alternatives --set iptables /usr/sbin/iptables-legacy
+update-alternatives --set ip6tables /usr/sbin/ip6tables-legacy
+
+mkdir -p /etc/systemd/system/docker.service.d
+
+cat <<EOF >> /etc/systemd/system/docker.service.d/socket.conf
+[Service]
+ExecStart=
+ExecStart=/usr/bin/dockerd -H fd:// -H tcp://0.0.0.0:2375
+EOF
+
+systemctl daemon-reload
+systemctl restart docker
